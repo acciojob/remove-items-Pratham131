6dc819b1a0bfd5ad21f4172e1b2fbae41e72d7dd
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-06-17 19:03:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeColor() {
+	let selectElement = document.getElementById("colorSelect")
+	let selectOption = selectElement.options[selectElement.selectedIndex]
+
+	if(selectOption){
+		selectElement.removeChild(selectOption)
+	}
+}
